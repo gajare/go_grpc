a6db1a816295ee7264a1080cb200e11ea81538fb
gajare <gajare147@gmail.com>
2025-06-24 22:33:31 +0530

    unary function for both client and server

--- server/unary.go
+++ server/unary.go
@@ -5,6 +5,7 @@ import (
 	pb "go_grpc/proto"
 )
 
+// unary server function
 func (s *helloServer) SayHello(ctx context.Context, req *pb.NoParam) (*pb.HelloResponse, error) {
 	return &pb.HelloResponse{
 		Message: "Hello",
